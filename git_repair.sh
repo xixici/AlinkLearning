a087b892526cfa7369baf717efe3f5a5fca8a335
xixici <202388990@qq.com>
2020-01-17 16:00:20 +0800

    fix: 修改项目账号
    由于账号冲突，修改了公司账号到github账号

---
+++ git_repair.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git filter-branch --env-filter '
+OLD_EMAIL="yang.lei01@eisoo.com"
+CORRECT_NAME="xixici"
+CORRECT_EMAIL="202388990@qq.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
